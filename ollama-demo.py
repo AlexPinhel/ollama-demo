5ae2dcc6ed7b041d256e192f042a95eb4bae2a43
Alexandre Pinhel <pinhel@amazon.fr>
2024-04-26 14:58:50 +0200

    update with streamlit

--- ollama-demo.py
+++ ollama-demo.py
@@ -1,13 +1,38 @@
-from langchain.llms import Ollama
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler                                  
+import ollama
+import streamlit as st
+import json
 
-llm = Ollama(model="llama2", 
-             callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
 
-ask = input("Que voulez-vous demander au chatbot?")
+def call_llm(modelsList, text):
+  response = ollama.chat(model=modelsList, messages=[
+    {
+      'role': 'user',
+      'content': text,
+    },
+  ])
+  return response['message']['content']
 
-#import sys
-#ask = sys.argv[1]
 
-llm(ask)
+@st.cache_resource(ttl=3600)
+def get_models():
+  mymodels = ollama.list()['models']
+  names = []
+  for model in mymodels:
+    names.append(model['name'].split(":")[0])
+  return names
+
+def main():
+  st.title("Chat locally")
+  st.write("Ask me anything!")
+
+  with st.form("my-form"):
+    modelsList = st.selectbox("Select a model", get_models())
+    text = st.text_input("Enter your question")
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+      #modelsList = 'llama2'
+      st.write(call_llm(modelsList, text))
+
+if __name__ == "__main__":
+    main()
